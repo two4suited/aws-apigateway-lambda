298e75c0841d42e4396e84a6ad18e2862882e385
Brian Sheridan <brian.sheridan@gmail.com>
2020-02-15 15:40:08 -0800

    Initial

---
+++ src/aws-gateway-lambda.infrastructure/AwsGatewayLambdaInfrastructureStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace AwsGatewayLambdaInfrastructure
+{
+    public class AwsGatewayLambdaInfrastructureStack : Stack
+    {
+        internal AwsGatewayLambdaInfrastructureStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
---
+++ src/aws-gateway-lambda.infrastructure/Program.cs
@@ -0,0 +1,17 @@
+﻿﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AwsGatewayLambdaInfrastructure
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new AwsGatewayLambdaInfrastructureStack(app, "AwsGatewayLambdaInfrastructureStack");
+            app.Synth();
+        }
+    }
+}
