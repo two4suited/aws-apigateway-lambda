4310daa53e5eaa050a36dbcb9c6e09ccbecef02c
Brian Sheridan <brian.sheridan@gmail.com>
2020-02-15 16:20:07 -0800

    Added Deployment of Function to CDK

--- src/aws-gateway-lambda.infrastructure/AwsGatewayLambdaInfrastructureStack.cs
+++ src/aws-gateway-lambda.infrastructure/AwsGatewayLambdaInfrastructureStack.cs
@@ -1,12 +1,23 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.Lambda;
 
-namespace AwsGatewayLambdaInfrastructure
+namespace aws
 {
     public class AwsGatewayLambdaInfrastructureStack : Stack
     {
         internal AwsGatewayLambdaInfrastructureStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            var getperson = new Function(this,"getperson", new FunctionProps()
+                {
+                    Runtime = Runtime.DOTNET_CORE_2_1,
+                    FunctionName = "TestAPI_GetPerson",
+                    Timeout = Duration.Minutes(1),
+                    MemorySize = 128,
+                    Code = Code.FromAsset("../aws-gateway-lambda.lambda/bin/Release/netcoreapp2.1/publish"),
+                    Handler = "aws-gateway-lambda.lambda::aws_gateway_lambda.lambda.Function::FunctionHandler"
+                }
+            );
+            
         }
     }
 }
