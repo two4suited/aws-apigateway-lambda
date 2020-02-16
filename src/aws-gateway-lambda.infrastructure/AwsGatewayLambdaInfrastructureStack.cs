3b8718a7821274c028093315a4470dc76451e437
Brian Sheridan <brian.sheridan@gmail.com>
2020-02-15 16:57:49 -0800

    formatting

--- src/aws-gateway-lambda.infrastructure/AwsGatewayLambdaInfrastructureStack.cs
+++ src/aws-gateway-lambda.infrastructure/AwsGatewayLambdaInfrastructureStack.cs
@@ -7,16 +7,14 @@ namespace aws
     {
         internal AwsGatewayLambdaInfrastructureStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            var getperson = new Function(this,"getperson", new FunctionProps()
-                {
+            var getperson = new Function(this,"getperson", new FunctionProps() {
                     Runtime = Runtime.DOTNET_CORE_2_1,
                     FunctionName = "TestAPI_GetPerson",
                     Timeout = Duration.Minutes(1),
                     MemorySize = 128,
                     Code = Code.FromAsset("../aws-gateway-lambda.lambda/bin/Release/netcoreapp2.1/publish"),
                     Handler = "aws-gateway-lambda.lambda::aws_gateway_lambda.lambda.Function::FunctionHandler"
-                }
-            );
+                });
             
         }
     }
